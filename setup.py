5706d4f8a41f15249d05923057313666cf76b48a
Logan Ripplinger <logan@brighthive.io>
2019-07-19 20:46:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='jsphr_converter',
     version='0.1',
-    packages=['jsphr_converter',],
+    packages=['jsphr_converter'],
     license='None',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
