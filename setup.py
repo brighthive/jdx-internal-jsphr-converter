53d2215d1185193a7b4b12285345a0137ea46c28
Logan Ripplinger <logan@brighthive.io>
2019-07-19 20:43:03 -0700

    Add setup.py for distro

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='jsphr_converter',
+    version='0.1',
+    packages=['jsphr_converter',],
+    license='None',
+    long_description=open('README.txt').read(),
+)
